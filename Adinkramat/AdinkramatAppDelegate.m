0f4e13b2a4b82aa1aba509eccac5f266904b9905
jp.business63 <jp.business63@ab59765a-77f6-29ee-75b7-c4a23a568648>
2011-02-27 07:29:41 +0000

    Created AdinkramatAppDelegate. Currently only included for dependency support.

---
+++ Adinkramat/AdinkramatAppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AdinkramatAppDelegate.m
+//  Adinkramatic
+//
+//  Created by James on 2/13/11.
+//  Copyright 2011 university of md college park. All rights reserved.
+//
+
+#import "AdinkramatAppDelegate.h"
+
+
+@implementation AdinkramatAppDelegate
+
+#pragma mark Operation Methods
+
+// JP - 2/26/11
+// Launches a window to multiply two adinkras together.
+- (IBAction)multiplyAdinkras:(id)sender {
+	NSLog(@"multiply");
+}
+
+// JP - 2/26/11
+// Launches a window to commutate two adinkras.
+- (IBAction)commutateAdinkras:(id)sender {
+	NSLog(@"commutate");
+}
+@end
