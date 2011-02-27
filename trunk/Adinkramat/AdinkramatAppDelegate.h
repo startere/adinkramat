43dcc067edefe3da748f4dd3916d80b9de8f2746
jp.business63 <jp.business63@ab59765a-77f6-29ee-75b7-c4a23a568648>
2011-02-27 07:29:41 +0000

    Created AdinkramatAppDelegate. Currently only included for dependency support.

---
+++ trunk/Adinkramat/AdinkramatAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AdinkramatAppDelegate.h
+//  Adinkramatic
+//
+//  Created by James on 2/13/11.
+//  Copyright 2011 university of md college park. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AdinkramatAppDelegate : NSObject {
+}
+
+// Operation Methods
+- (IBAction)multiplyAdinkras:(id)sender;
+- (IBAction)commutateAdinkras:(id)sender;
+
+@end
